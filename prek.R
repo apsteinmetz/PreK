7e7ddf4b90856cd3e186a6536eb4eb405121c2e5
apsteinmetz <apsteinmetz@yahoo.com>
2017-01-12 17:14:54 -0500

    included all pre-k cleaned data as a CSV

--- prek.R
+++ prek.R
@@ -14,7 +14,7 @@ library(cowplot)
 #see acs package documentation
 #api.key.install("your key here")
 
-############################################################################################
+# -----------------------------------------------------------------------
 # get census data on children and income
 # NYC county codes
 nyc_fips = c(36085,36005, 36047, 36061, 36081)
@@ -22,7 +22,7 @@ nyc_fips = c(36085,36005, 36047, 36061, 36081)
 data("zip.regions")
 nyc_zips<-data.frame(county.fips.numeric=nyc_fips)%>%inner_join(zip.regions)%>%select(region)%>%t
 # make an ACS geo set
-nycgeo<- geo.make(zip.code = nyc_zips,check =T)
+nycgeo<- geo.make(zip.code = nyc_zips)
 
 income<-acs.fetch(endyear=2011,geography=nycgeo,table.number="B19301")
 #put acs data in a form for rendering
@@ -46,12 +46,14 @@ names(pop)<-c("NAME","zip","totPop")
 pop<-pop%>%select(zip,totPop)%>%distinct()%>%filter(totPop!=0)
 
 #where are zips with the most rugrats?
-kidsChor <- pop%>%inner_join(kids)%>%transmute(region=zip,value=kidsUnder3/totPop)
-zip_choropleth(kidsChor,zip_zoom = nyc_zips,title = "Number of Pre-K Seats")
+kidsChor <- pop%>%inner_join(kids)%>%transmute(region=zip,value=kidsUnder3/totPop*100)
+zip_choropleth(kidsChor,zip_zoom = nyc_zips,title = "Percentage of Kids Under 3 in 2011")
 
 # if you want an underlying map and are using version 1.4 of zip_choropleth
 #zip_choropleth(kidsChor,zip_zoom = nyc_zips,title = "Number of Pre-K Seats",reference_map = TRUE)
-######################################################################################
+
+# -----------------------------------------------------------------------
+# get NYC data on pre-K programs
 # scan seat directory pdfs and put into a data frame by zip code
 #DOE pre-k directories
 urls<- c("http://schools.nyc.gov/NR/rdonlyres/1F829192-ABE8-4BE6-93B5-1A33A6CCC32E/0/2015PreKDirectoryManhattan.pdf",
@@ -87,30 +89,21 @@ txt2<-sub("'","",txt2)
 seats<-as.data.frame(str_match(txt2,pkseattokens))[,c(4,6,7)]
 names(seats)<-c("zip","seats","daylength")
 seats$seats<-as.integer(seats$seats)
+# aggregate seat count by zip code
+sumSeats<-seats%>%group_by(zip)%>%summarise(count=n(),numSeats=sum(seats,na.rm=TRUE))
+names(sumSeats)<-c("zip","schools","numSeats")
 
 
 #how do the programs break out in terms of day length?
 sumDayLength<-seats%>%group_by(daylength)%>%summarise(NumSchools=n(),NumSeats=sum(seats,na.rm=TRUE))
 print(sumDayLength)
 
-# aggregate seat count by zip code
-sumSeats<-seats%>%group_by(zip)%>%summarise(count=n(),numSeats=sum(seats,na.rm=TRUE))
 
-# some preliminary pictures
-sumSeatsChor<-sumSeats
-names(sumSeatsChor)<-c("region","value","numSeats")
-zip_choropleth(sumSeatsChor,zip_zoom = nyc_zips,title = "Number of Schools")
-names(sumSeatsChor)<-c("region","Schools","value")
-zip_choropleth(sumSeatsChor,zip_zoom = nyc_zips,title = "Number of Pre-K Seats")
-
-################################################################################################
+# -----------------------------------------------------------------------
 #combine the vectors for seats, income and population
-# data frame including new seats not working yet
-#allData<-sumSeats%>%join(pop)%>%join(kids)%>%join(inc)%>%join(aggNew)
 allData<-sumSeats%>%join(pop)%>%join(kids)%>%join(inc)%>%na.omit()
-#get rid of airports
+#get rid of airports, JFK and LGA
 allData<-filter(allData,zip!=11371 & zip!=11430)
-
 
 # add normalized seats per capita/kid
 allData<-mutate(allData,seatsPer100Kids = numSeats/ kidsUnder3*100,seatsPerCapita=numSeats/totPop)
@@ -122,15 +115,33 @@ allData<-mutate(allData,incomeQuantile=fn(allData$perCapitaIncome))
 #compute seatsPer100Kids quantiles
 fn<-ecdf(allData$seatsPer100Kids)
 allData<-mutate(allData,seatsQuantile=fn(allData$seatsPer100Kids))
-
-#is there an obvious relationship between income and seats?
-plot(allData$seatsPer100Kids,allData$perCapitaIncome)
-# plot(allData$newSeats,allData$perCapitaIncome)
-
 # set bins for bi-variate plot
 bins<-3
 allData<-mutate(allData,seatsBin=cut2(seatsPer100Kids,g=bins,levels.mean = TRUE))
 allData<-mutate(allData,incomeBin=cut2(perCapitaIncome,g=bins,levels.mean = TRUE))
+
+#Write out allData so we don't have to scrape the city web data each time
+# and we can make the analysis self-contained
+
+write.csv(allData,file='PreKData.csv',row.names = FALSE)
+# ------------------------------------------------------------------------
+# end of web scraping and data prepping
+# -------------------------------------------------------------------------
+
+# load data to analyze
+allData<-read.csv(file='PreKData.csv')
+
+
+# some preliminary pictures
+allData%>%transmute(region=zip,value=schools)%>%zip_choropleth(zip_zoom = nyc_zips,title = "Number of Schools")
+allData%>%transmute(region=zip,value=numSeats)%>%zip_choropleth(zip_zoom = nyc_zips,title = "Number of Pre-K Seats")
+
+#is there an obvious relationship between income and seats?
+ggplot(allData,aes(y=seatsPer100Kids,x=perCapitaIncome))+geom_point()
+#lets look at the same data normalized by quantile
+#no obvious relationship
+ggplot(allData,aes(y=seatsQuantile,x=incomeQuantile))+geom_point()
+
 
 # create a data frame exclusively for use in a chorpleth object
 # contains only zips as "region" and income/seats crosstab as "value"
@@ -159,6 +170,7 @@ gg<-bvc$render() + theme(legend.position="none")
 #gg<-ggdraw(gg) + draw_text(text = "More Seats -->",x=0.84,y=0.49,angle=270)
 
 #use cowplot to create a plot that will be the legend itself
+#first create a legend plot
 legendGoal=melt(matrix(1:9,nrow=3))
 lg<-ggplot(legendGoal, aes(Var2,Var1,fill = as.factor(value)))+ geom_tile()
 lg<- lg + scale_fill_manual(name="",values=bvColors)
@@ -179,6 +191,7 @@ names(hm)<-c("SeatsPer100Kids","IncomePerCapita","Freq")
 #display cross-tab
 print(xtab)
 #show heatmap of crosstab
+# this suggests that high income zip codes are underserved with pre-K seats
 ggplot(hm, aes(SeatsPer100Kids, IncomePerCapita)) + geom_tile(aes(fill = Freq),colour = "white") +
-       scale_fill_gradient(low = "white", high = "steelblue")
+       scale_fill_gradient(low = "steelblue", high = "white")
 
