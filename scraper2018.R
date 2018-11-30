8911852f2c446355a25feef59ffd2c76300d82ab
apsteinmetz <apsteinmetz@yahoo.com>
2018-11-30 18:07:31 -0500

    create tibble for all schools

--- scraper2018.R
+++ scraper2018.R
@@ -18,14 +18,16 @@ urls<-c("https://www.schools.nyc.gov/docs/default-source/default-document-librar
 boroughs <- c('Bronx','Brooklyn','Manhattan','Queens','Staten')
 
 #regex to parse address line
-pkseattokens <-"(Address: )([.[:alnum:]- ()]+),+ ([0-9]{5})([a-zA-Z .()-:]+) ([0-9]{1,4}) (FD|HD|AM|PM|5H)"
+#pkseattokens <-"(Address: )([.[:alnum:]- ()]+),+ ([0-9]{5})([a-zA-Z .()-:]+) ([0-9]{1,4}) (FD|HD|AM|PM|5H)"
 
 dests <- paste0("pdf/",boroughs,"2018_Pre_K.pdf")
 
+# Download PDF directories from NYC
 for (i in 1:length(urls)) {
   download.file(urls[i],destfile = dests[i],mode = "wb")
 }
 
+# extract and combine text
 listings <- NULL
 for (i in 1:length(dests)) {
   print(dests[i])
@@ -45,15 +47,17 @@ addresstokens <-"(Address: )[0-9 A-Za-z]+,[0-9 A-Za-z]+([0-9]{5})"
 #use lines containing address as anchor to mark record boundaries
 lines_address<-grep(addresstokens,listings)
 record_start<-lines_address-1
-# record_end<-(lines_address-2)[-1] %>% append(length(lines_record_start))
-# record_boundaries <- data_frame(start=record_start,end=record_end)
+record_end<-(lines_address-2)[-1] %>% append(length(record_start))
+record_boundaries <- data_frame(start=record_start,end=record_end)
 
-create_record <- function(rec){
+create_record <- function(rec_index){
+  rec <- listings[record_boundaries[rec_index,]$start:record_boundaries[rec_index,]$end]
   name <- str_extract(rec[1],"(.+)(?=\\|)") %>% str_trim()
   address <- str_extract(rec[2],addresstokens)%>% str_remove("Address: ")
   zip <- str_extract(address,"[0-9]{5}$")
-  day_length <- rec[3] %>% str_extract("([A-Za-z]+)(?=-Day)")
-  seats = str_extract(tmp,"(?<=Seats: )[0-9]+") %>% na.omit() %>% as.numeric()
+  # the next two fields should be tolerant of being located on variant lines
+  day_length <- str_extract(rec,"([A-Za-z]+)(?=-Day)")  %>% na.omit() %>% as.character() %>% .[1]
+  seats = str_extract(rec,"(?<=Seats: )[0-9]+") %>% na.omit() %>% as.numeric() %>% .[1]
   return(data_frame(name=name,
                     address=address,
                     zip=zip,
@@ -61,8 +65,9 @@ create_record <- function(rec){
                     day_length=day_length))
   
 }
-create_record(listings[record_boundaries[100,]$start:record_boundaries[100,]$end])
+1:10 %>% map(create_record) %>% bind_rows()
 
+map(record_boundaries[1:10,],function(x)create_record(listings[x$start:x$end]))
 # find address line which contains zip and seat count
 listings2 <- listings %>% str_split("\\r?\\n") %>% unlist()
 addresses <- str_extract_all(listings2,addresstokens) %>% unlist() %>% str_remove("Address: ")
@@ -80,3 +85,9 @@ sumSeats <- schools %>%
   summarise(count = n(), 
             numSeats = sum(seats, na.rm = TRUE))
 names(sumSeats)<-c("zip","schools","numSeats")
+
+mtcars %>%
+  split(.$cyl) %>%
+  map(~ lm(mpg ~ wt, data = .x)) %>%
+  map(summary) %>%
+  map_dbl("r.squared")
